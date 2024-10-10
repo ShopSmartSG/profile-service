89e3de702affab0ee5bf4120b02fb3b7226abd5f
Anvisimi <8444.sa@gmail.com>
2024-10-10 23:14:09 +0800

    added location url parameter

--- src/main/java/sg/edu/nus/iss/profile_service/factory/ExternalLocationService.java
+++ src/main/java/sg/edu/nus/iss/profile_service/factory/ExternalLocationService.java
@@ -1,14 +1,19 @@
 package sg.edu.nus.iss.profile_service.factory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import sg.edu.nus.iss.profile_service.model.LatLng;
 
 @Service
 public class ExternalLocationService {
+    @Value("${location.service.url}")
+    private String locationServiceUrl;
 
     private final RestTemplate restTemplate;
+
+
 
     @Autowired
     public ExternalLocationService(RestTemplate restTemplate) {
@@ -16,7 +21,7 @@ public class ExternalLocationService {
     }
 
     public LatLng getCoordinates(String pincode) {
-        String url = "http://localhost:8083/location/coordinates?pincode=" + pincode;
+        String url = locationServiceUrl+"/location/coordinates?pincode=" + pincode;
         try {
             return restTemplate.getForObject(url, LatLng.class);
         } catch (Exception e) {
