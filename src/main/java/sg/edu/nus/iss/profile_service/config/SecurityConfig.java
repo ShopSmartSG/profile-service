84e0259683faa1cbacbcfce830a86f49f46d5a47
Anvisimi <8444.sa@gmail.com>
2025-02-27 23:03:26 +0800

    made changes for taking mock repo beans when db not up/connected test

--- src/main/java/sg/edu/nus/iss/profile_service/config/SecurityConfig.java
+++ src/main/java/sg/edu/nus/iss/profile_service/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package sg.edu.nus.iss.profile_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@Profile("!test")
 public class SecurityConfig {
 
     @Bean
