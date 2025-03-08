2f045880cd563db963f7c37f2b084eb52b8f7395
Anvisimi <8444.sa@gmail.com>
2025-03-08 12:55:03 +0800

    reverted as local changes were pushed to branch

--- src/main/java/sg/edu/nus/iss/profile_service/model/Customer.java
+++ src/main/java/sg/edu/nus/iss/profile_service/model/Customer.java
@@ -23,26 +23,26 @@ public class Customer implements Profile {
     private UUID customerId;
 
     @NotBlank(message = "Customer name is mandatory")
-    
+    @Convert(converter = StringEncryptionConverter.class)
     private String name;
 
     @NotBlank(message = "Customer email is mandatory")
     @Email(message = "Email should be valid")
-    
+    @Convert(converter = StringEncryptionConverter.class)
     private String emailAddress;
 
-    
+    @Convert(converter = StringEncryptionConverter.class)
     private String addressLine1;
 
-    
+    @Convert(converter = StringEncryptionConverter.class)
     private String addressLine2;
 
     @Pattern(regexp = "^\\+?[0-9. ()-]{7,25}$", message = "Phone number is invalid")
-    
+    @Convert(converter = StringEncryptionConverter.class)
     private String phoneNumber;
 
     @Pattern(regexp = "^[0-9]{6}$", message = "Pincode must be a 6-digit number")
-    
+    @Convert(converter = StringEncryptionConverter.class)
     private String pincode;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
